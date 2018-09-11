01fdb94b2c038b116867f69e258214b509563219
lingchao2016 <lingchao87@gmail.com>
2018-09-10 22:33:55 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+
 const app = express();
 
-app.get('/', (req, res) =>{
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+//clientid: 1009571969294-gmhmvti6o650reikvmahub491f441974.apps.googleusercontent.com
+//client secret: dv7S2KrbAHKyqxtMaWU5immr
+
+/*app.get('/', (req, res) =>{
   res.send({bye: 'buddy'});
 });
 
+
+*/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
