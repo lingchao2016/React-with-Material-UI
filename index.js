de0cf7e9bdd1320a09f316429eca59d64e0cb211
lingchao2016 <lingchao87@gmail.com>
2018-09-16 13:48:16 -0400

    fix another bug

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if( process.env.NODE_ENV === ' production'){
+if( process.env.NODE_ENV === 'production'){
   // Express will serve up production assets
   app.use(express.static('client/build'));
 
